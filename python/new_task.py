6215e8a1a4eafd0f8cbe6523e386adfdb3c9b126
NOTMOVETON <97618317+NOTMOVETON@users.noreply.github.com>
2025-12-06 01:09:23 +0300

    я тут накакал)

---
+++ python/new_task.py
@@ -0,0 +1,18 @@
+grades = []
+
+for i in range(4):
+    x = int(input())
+    grades.append(x)
+
+
+avg = sum(grades)/len(grades)
+
+if avg >= 4:
+    print('Результат: отлично')
+    
+elif 3 <= avg <= 4:
+    print('Результат: хорошо')
+    
+else:
+    print('Результат удовлетворительно')
+    
